6099765e728edac757d588b5fb6760deaf7c0852
fcicerelli <fcicerellitic@gmail.com>
2025-01-31 18:00:58 -0300

    clase05 completada

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 ( () => {
 
 const btn = document.querySelector("[data-form-btn]");
@@ -11,33 +14,18 @@ const createTask = (evento) => {
   task.classList.add('card');
   input.value = '';
   const taskContent = document.createElement('div');
-  taskContent.appendChild(checkComplete());
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerHTML = value;
+  taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
-  const content = `
-        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  
   //task.innerHTML = content;
   task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
   list.appendChild(task);
 }
 
 btn.addEventListener('click', createTask);
-
-const checkComplete = () => {
-  const i = document.createElement('i');
-  i.classList.add("far", "fa-check-square", "icon");
-  i.addEventListener('click', completeTask);
-  return i;
-}
-
-
-const completeTask = (event) => {
-  const element = event.target;
-  element.classList.toggle('fas');
-  element.classList.toggle('completeIcon');
-  element.classList.toggle('far');
-}
 
 })();
